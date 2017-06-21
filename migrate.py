1ac8e6fc289d6819aa7b2a4b6046427a88133706
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-06-21 11:39:24 +0100

    For every repo, do the initial clone

--- migrate.py
+++ migrate.py
@@ -1,12 +1,16 @@
 import subprocess
-import os
 import sys
+import os
 
-url = sys.argv[1]
+URL = sys.argv[1]
+
+def create_log_folder():
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
 
 def cmd_exists(cmd):
-    return subprocess.call("type " + cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
-
+    return subprocess.call("type " + cmd, shell=True, stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE) == 0
 
 def verify_environment():
     if not cmd_exists("java"):
@@ -20,6 +24,20 @@ def list_repositories(url):
     repository_list = repositories.split('/\n')
     return filter(None, repository_list)
 
-for repo in list_repositories(url):
-    print repo
+def migrate_repo(url, repo):
+    clone(repo, url)
 
+def clone(url, repo):
+    output_log = open("logs/{0}.log".format(repo), "ab")
+    repo_url = "{0}/{1}".format(url, repo)
+    command = ["git", "svn", "clone", "--stdlayout", "--authors-file=authors.txt", repo_url, repo]
+    print " ".join(command)
+    subprocess.check_call(command, stdout=output_log, stderr=output_log)
+
+def main():
+    create_log_folder()
+    repositories = list_repositories(URL)
+    for repository in repositories:
+        clone(URL, repository)
+
+main()
