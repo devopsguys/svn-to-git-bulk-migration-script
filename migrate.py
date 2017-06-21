991d21817fd5a8368ef7d225fa21fc015fe9d3e2
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-06-21 15:44:46 +0100

    Refactoring and error handling

--- migrate.py
+++ migrate.py
@@ -1,6 +1,13 @@
 import subprocess
 import os
+import sys
 import yaml
+
+
+def run_command(command, repo, cwd=sys.path[0]):
+    output_log = open("logs/{0}.log".format(repo), "ab")
+    print "Running command {0}".format(" ".join(command))
+    subprocess.check_call(command, stdout=output_log, stderr=output_log, cwd=cwd)
 
 def get_repo_name_from_url(url):
     repo = filter(None, url.split("/"))
@@ -27,25 +34,34 @@ def list_repositories():
     repositories_yml.close()
     return repositories
 
-def clone(url, trunk, tags, branches):
-    repo = get_repo_name_from_url(url)
-    output_log = open("logs/{0}.log".format(repo), "ab")
-    command = ["git", "svn", "clone", "--stdlayout", "--authors-file=authors.txt", url, repo,
-               "--trunk='{0}'".format(trunk), "--tags='{0}'".format(tags),
-               "--branches='{0}'".format(branches)]
-    subprocess.check_call(command, stdout=output_log, stderr=output_log)
+def git_svn_init(url, repo, trunk, tags, branches):
+    init_command = ["git", "svn", "init", url, repo,
+                    "--trunk={0}".format(trunk), "--tags={0}".format(tags),
+                    "--branches={0}".format(branches)]
+    run_command(init_command, repo)
 
-def clean(repo):
-    output_log = open("logs/{0}.log".format(repo), "ab")
-    command = ["java", "-Dfile.encoding=utf-8", "-jar", "../svn-migration-scripts.jar",
-               "clean-git", "--force"]
-    subprocess.check_call(command, stdout=output_log, stderr=output_log, cwd=repo)
+def git_svn_fetch(repo):
+    fetch_command = ["git", "svn", "fetch", "--authors-file=../authors.txt"]
+    run_command(fetch_command, repo, repo)
+
+def git_svn_clean(repo):
+    clean_command = ["java", "-Dfile.encoding=utf-8", "-jar", "../svn-migration-scripts.jar",
+                     "clean-git", "--force"]
+    try:
+        run_command(clean_command, repo, repo)
+    except subprocess.CalledProcessError:
+        print "Error generated while cleaning repository {0}".format(repo)
 
 def migrate_repo(repository):
     trunk = repository.get("trunk") or "trunk"
     tags = repository.get("tags") or "tags"
     branches = repository.get("branches") or "branches"
-    clone(repository, trunk, tags, branches)
+    url = repository.get("repo")
+    repo = get_repo_name_from_url(url)
+    if not os.path.isdir(repo):
+        git_svn_init(url, repo, trunk, tags, branches)
+    git_svn_fetch(repo)
+    git_svn_clean(repo)
 
 def main():
     create_log_folder()
