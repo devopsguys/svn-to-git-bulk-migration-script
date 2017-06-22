1a2da7dfe10a2c769cb0a2815d3c55804797f011
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-06-22 16:57:37 +0100

    Fix the command that cleans up the git repo

--- migrate.py
+++ migrate.py
@@ -37,8 +37,10 @@ def load_yaml_file(path):
     yaml_file.close()
     return dataset
 
+# --prefix= added so that the default branch mapping behaviour from git 1.x is restored
+# Without this, the cleanup script at the end won't work!
 def git_svn_init(url, repo, trunk, tags, branches):
-    init_command = ["git", "svn", "init", url, repo,
+    init_command = ["git", "svn", "init", url, repo, '--prefix=',
                     "--trunk={0}".format(trunk), "--tags={0}".format(tags),
                     "--branches={0}".format(branches)]
     run_command(init_command, repo, MIGRATION_DIR)
@@ -48,55 +50,12 @@ def git_svn_fetch(repo):
     run_command(fetch_command, repo, os.path.join(MIGRATION_DIR, repo))
 
 def git_svn_clean(repo):
-    clean_command = ["java", "-Dfile.encoding=utf-8", "-jar", "../svn-migration-scripts.jar",
+    clean_command = ["java", "-Dfile.encoding=utf-8", "-jar", "../../svn-migration-scripts.jar",
                      "clean-git", "--force"]
     try:
         run_command(clean_command, repo, os.path.join(MIGRATION_DIR, repo))
     except subprocess.CalledProcessError:
         print "Error generated while cleaning repository {0}".format(repo)
-
-def git_checkout_all_branches(repo):
-    branches = subprocess.check_output(["git", "branch", "-r"], cwd=os.path.join(MIGRATION_DIR, repo))
-    branches = filter(None, branches.split("\n"))
-    strip_spaces = (lambda x: x.strip())
-    no_tags = (lambda x: "origin/tags/" not in x)
-    no_trunk = (lambda x: x != 'origin/trunk')
-    branches = map(strip_spaces, branches)
-    branches = filter(no_tags, branches)
-    branches = filter(no_trunk, branches)
-    for branch in branches:
-        try:
-            reset_branch = ["git", "reset", "--hard", "HEAD"]
-            checkout_branch = ["git", "checkout", "-b", branch.replace('origin/', ''), branch]
-            run_command(reset_branch, repo, os.path.join(MIGRATION_DIR, repo))
-            run_command(checkout_branch, repo, os.path.join(MIGRATION_DIR, repo))
-        except:
-            pass
-
-    run_command(["git", "reset", "--hard", "HEAD"], repo, os.path.join(MIGRATION_DIR, repo))
-    run_command(["git", "checkout", "master"], repo, os.path.join(MIGRATION_DIR, repo))
-
-# def git_checkout_all_branches(repo):
-#     branches = subprocess.check_output(["git", "branch", "-r"], cwd=os.path.join(MIGRATION_DIR, repo))
-#     branches = filter(None, branches.split("\n"))
-#     strip_spaces = (lambda x: x.strip())
-#     no_tags = (lambda x: "origin/tags/" not in x)
-#     no_trunk = (lambda x: x != 'origin/trunk')
-#     branches = map(strip_spaces, branches)
-#     branches = filter(no_tags, branches)
-#     branches = filter(no_trunk, branches)
-#     for branch in branches:
-#         try:
-#             reset_branch = ["git", "reset", "--hard", "HEAD"]
-#             checkout_branch = ["git", "checkout", "-b", branch.replace('origin/', ''), branch]
-#             run_command(reset_branch, repo, os.path.join(MIGRATION_DIR, repo))
-#             run_command(checkout_branch, repo, os.path.join(MIGRATION_DIR, repo))
-#         except:
-#             pass
-
-#     run_command(["git", "reset", "--hard", "HEAD"], repo, os.path.join(MIGRATION_DIR, repo))
-#     run_command(["git", "checkout", "master"], repo, os.path.join(MIGRATION_DIR, repo))
-
 
 def migrate_repo(repository):
     trunk = repository.get("trunk") or "trunk"
@@ -108,7 +67,6 @@ def migrate_repo(repository):
         git_svn_init(url, repo, trunk, tags, branches)
     git_svn_fetch(repo)
     git_svn_clean(repo)
-    # git_checkout_all_branches(repo)
 
 def main():
     mkdir_p(LOGS_DIR)
